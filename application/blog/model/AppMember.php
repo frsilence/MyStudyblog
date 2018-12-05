ef322366aeacf4f2eed1c5d460af99117363c790
frsilence <frsilence>
2018-12-05 17:25:28 +0800

    1

--- application/blog/model/AppMember.php
+++ application/blog/model/AppMember.php
@@ -4,7 +4,18 @@ namespace app\blog\model;
 
 use think\Model;
 
+/**
+ * 应用用户模型
+ */
 class AppMember extends Model
 {
-    //
+    protected $table = 'app_member';
+
+    /**
+     * 所有用户基本信息获取
+     */
+    public function getAppMemberInfo()
+    {
+    	return $this->field('id,member_pid,username,userimage,phone,email,province,city,sex,status')->find();
+    }
 }
