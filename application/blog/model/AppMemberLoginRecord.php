7b23458de6c260120480b4db50125a23ff1875d3
frsilence <frsilence>
2018-12-06 17:25:18 +0800

    2018-12-6 17:25:14

--- application/blog/model/AppMemberLoginRecord.php
+++ application/blog/model/AppMemberLoginRecord.php
@@ -4,7 +4,33 @@ namespace app\blog\model;
 
 use think\Model;
 
+/**
+ * 登录记录
+ */
 class AppMemberLoginRecord extends Model
 {
-    //
+    protected $table = 'app_member_login_record';
+
+    /**
+     * 获取用户登录记录
+     * @param  int $member 指定用户id
+     * @param  int $page 每页获取数量
+     * @reurn \think\Paginator 
+     */
+    public function getMemberLoginRecord($member_id,$page=15)
+    {
+    	$login_record = $this->where(['member_id'=>$member_id])->order('create_at','desc')->paginate($page);
+    	return $login_record;
+    }
+
+    /**
+     * 获取上次登录时间 (用于登录过期检测)
+     * @param int $member
+     * @return  string
+     */
+    public function getLastLoginTime($member_id)
+    {
+    	$login_record = $this->where(['member_id'=>$member_id,'type'=>0])->order(['create_at'=>'desc'])->limit(1)->select();
+    	(isset($login_record[0])) ? return $login_record[0]['create_at'] : return '';
+    }
 }
--- application/blog/model/Article.php
+++ application/blog/model/Article.php
@@ -4,7 +4,31 @@ namespace app\blog\model;
 
 use think\Model;
 
+/**
+ * 文章数据表模型
+ */
+
 class Article extends Model
 {
-    //
+    protected $table = 'article';
+
+    /**
+     * 增加文章
+     * @param  array $article_info 文章信息
+     * @return  int article_id
+     */
+    public function addArticle($article_info)
+    {
+    	$this->startTrans();
+    	try{
+    		$article=$this->save($article_info);
+    		$article_id = $this->article_id;
+    		$this->commit();
+    	} catch(/Exception $e){
+    		$this->rollback;
+    		return false;
+    	}
+    	$article_id = $this->article_id;
+
+    }
 }
